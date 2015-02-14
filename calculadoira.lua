c23c21c688940a1ffa68e9b20cbc77924f89d301
Christophe Delord <cdsoft.fr>
2015-02-14 08:52:01 +0100

    Bug fix in calculadoira.ini
    binary number syntax fixed.
    
    calculadoira.ini is also converted in DOS format

--- calculadoira.lua
+++ calculadoira.lua
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with Calculadoira.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-version = "3.0.6"
+version = "3.0.7"
 
 help = ([[
 +---------------------------------------------------------------------+
